a5e48a95f625c53b24894253679b6e6ce309a7b0
Ankur Malviya <madanankurmalviya@gmail.com>
2023-12-15 01:00:32 -0800

    Added AmazonTest

--- src/test/java/com/epam/healenium/tests/AmazonTest.java
+++ src/test/java/com/epam/healenium/tests/AmazonTest.java
@@ -1,5 +1,4 @@
 package com.epam.healenium.tests;
-
 import com.epam.healenium.FrameworkPage;
 import com.epam.healenium.constants.BrowserType;
 import com.epam.healenium.constants.DriverType;
@@ -11,20 +10,16 @@ import org.openqa.selenium.WebDriver;
 
 import java.net.MalformedURLException;
 import java.util.HashMap;
-
-
-public class BaseTest {
+public class AmazonTest {
     static protected WebDriver driver;
-    static protected HashMap<PagesType, FrameworkPage> pages;
-    public static void main (String []args) throws MalformedURLException {
+    public static void main(String[] args) throws MalformedURLException {
 
         driver = new DriverContext(DriverType.LOCAL).getDriver(BrowserType.CHROME);
         driver.manage().window().setSize(new Dimension(1200, 800));
-        driver.get("https://www.google.com");
-        driver.findElement(By.xpath("//*[@class='gLFyf']")).sendKeys("Hello");
-        driver.findElement(By.xpath("//a[text()='About']")).click();
+        driver.get("https://www.amazon.com");
+        driver.findElement(By.xpath("//input[@id='twotabsearchtextbox']")).sendKeys("Apple Magic Keyboard");
+        driver.findElement(By.xpath("//input[@type='submit']")).click();
         driver.quit();
 
     }
-
 }
